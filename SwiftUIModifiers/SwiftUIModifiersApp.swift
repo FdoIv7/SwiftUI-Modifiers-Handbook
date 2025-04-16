51f1c80641e0ed5dba588096a70e11f2c2e0a86b
Fernando Perez <fdoiv7@gmail.com>
2025-04-16 04:10:28 -0600

    Initial Commit

---
+++ SwiftUIModifiers/SwiftUIModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIModifiersApp.swift
+//  SwiftUIModifiers
+//
+//  Created by Fernando Ives on 16/04/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIModifiersTests/SwiftUIModifiersTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIModifiersTests.swift
+//  SwiftUIModifiersTests
+//
+//  Created by Fernando Ives on 16/04/25.
+//
+
+import Testing
+@testable import SwiftUIModifiers
+
+struct SwiftUIModifiersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
